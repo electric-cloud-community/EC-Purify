cf4f1df306ed12a8a3bea171e30e4e4f9cef2497
Valentyn Solomko <Valentyn.Solomko@portaone.com>
2019-01-08 15:43:20 +0200

    Adapt for building with Gradle

--- src/main/resources/project/manifest.pl
+++
@@ -1,5 +0,0 @@
-@files = (
-    ['//property[propertyName="purifyDriver"]/value', 'purifyDriver.pl'],
-    ['//property[propertyName="ec_setup"]/value', 'ec_setup.pl'],
-	['//procedure[procedureName="runPurify"]/propertySheet/property[propertyName="ec_parameterForm"]/value', 'runPurifyForm.xml'],
-);
